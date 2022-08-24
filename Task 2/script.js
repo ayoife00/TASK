12254ace983ce59ca259456bac2d199cb882ddec
ayoife00 <olalereolatunbosun@gmail.com>
2022-08-24 11:42:00 -0700

    comment added

--- Task 2/script.js
+++ Task 2/script.js
@@ -5,7 +5,7 @@ let largest = 0;
 //Create a for loop
 for (let i = 0; i < number.length; i++) {
     const value = number[i];
-
+//Use an if statement to check for the largest number
     if (value > largest){
         largest = value;
     }
